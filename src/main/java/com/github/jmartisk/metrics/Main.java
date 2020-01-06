4143db9a52066d6ac170379cca6649164b0a58ba
Jan Martiska <jmartisk@redhat.com>
2020-01-06 10:38:38 +0100

    log the startup duration

--- src/main/java/com/github/jmartisk/metrics/Main.java
+++ src/main/java/com/github/jmartisk/metrics/Main.java
@@ -4,10 +4,13 @@ import io.smallrye.metrics.MetricsRequestHandler;
 import io.vertx.core.Vertx;
 
 import javax.enterprise.inject.spi.CDI;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
+        Long start = System.currentTimeMillis();
+
         // start CDI and scan for metrics
         org.jboss.weld.environment.se.StartMain.main(null);
 
@@ -15,6 +18,8 @@ public class Main {
         Vertx vertx = Vertx.vertx();
         MetricsRequestHandler internalHandler = new MetricsRequestHandler();
         vertx.createHttpServer().requestHandler(new Handler(internalHandler)).listen(8080);
+
+        Logger.getLogger(Main.class.getName()).info("Started in " + (System.currentTimeMillis() - start) + " ms");
 
         // generate some actual data for start
         CDI.current().select(MetricBean.class).get().countedMethod();
